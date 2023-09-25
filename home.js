0d1ec5ee6d7df283f2c1579e9c209ee02908da49
arunkumar <kravikant249@gmail.com>
2023-09-25 11:15:40 +0530

    CertificationExam

---
+++ home.js
@@ -0,0 +1,5 @@
+let searchbtn = document.getElementById("searchbtn");
+searchbtn.addEventListener(Click,function(e){
+    console.log("search btn clicked");
+
+});
---
+++ teaminfo/teaminfo.js
@@ -0,0 +1,100 @@
+
+function redirectToCskPage() {
+    window.location.href = 'csk.html';
+}
+
+const TeamsInfo =[
+    {   id:1,
+        name:"Chennai Super kings",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/CSKoutline.png?alt=media&token=85f66811-b64d-4c80-9ff5-ffeb85a5c1d7",
+        captain:"Ms. Dhoni",
+        NoOfPlayers:32,
+        Trophies:5
+
+    },
+    {   id:2,
+        name:"Mumbai Indians",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Rohit Sharma",
+        NoOfPlayers:42,
+        Trophies:5
+
+    },
+    
+    {   id:3,
+        name:"Delhi Capital",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Rishabhi Pant",
+        NoOfPlayers:22,
+        Trophies:0
+
+    },
+    {   id:4,
+        name:"Punjab Kings",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Mayank Agrawal",
+        NoOfPlayers:29,
+        Trophies:0
+
+    },
+    {   id:5,
+        name:"Gujrat Titans",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Hardik Pandya",
+        NoOfPlayers:23,
+        Trophies:1
+
+    },
+
+    {   id:6,
+        name:"Lucknow Super Giants",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Kl Rahul",
+        NoOfPlayers:42,
+        Trophies:0
+
+    },
+    {   id:7,
+        name:"Royal Challenger Banglore",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Faf Duplesis",
+        NoOfPlayers:23,
+        Trophies:0
+
+    },
+    {   id:8,
+        name:"Rajsthan Royals",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Sanju Samson",
+        NoOfPlayers:37,
+        Trophies:1
+
+    },
+    {   id:9,
+        name:"Sun Rises Hydrabad",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Aiden Makram",
+        NoOfPlayers:34,
+        Trophies:2
+
+    },
+    {   id:10,
+        name:"Kolkata Knight Riders",
+        logo:"https://firebasestorage.googleapis.com/v0/b/iplteaminfowebapp.appspot.com/o/MIoutline.png?alt=media&token=ca0033a0-1d67-4093-acd3-7d5b38eae572",
+        captain:"Nitish Rana",
+        NoOfPlayers:27,
+        Trophies:2
+
+    }
+
+
+
+
+]
+
+
+
+
+
+
+
